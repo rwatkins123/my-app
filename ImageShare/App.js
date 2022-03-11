2cc336c332d79eb3a4c3e859de9e9c28d4d10b44
Rob Watkins <rwatkins123@gmail.com>
2022-03-11 10:10:10 -0600

    added imageShare project

---
+++ ImageShare/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'blue', fontSize: 20}}>
+        To share a photo from your phone with a friend, just press the button below!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
