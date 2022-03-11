0d2bc79cad1ebb049811d9c982f90d9ec1f20abb
Rob Watkins <rwatkins123@gmail.com>
2022-03-11 11:28:18 -0600

    added upload button

--- ImageShare/App.js
+++ ImageShare/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -8,6 +8,13 @@ export default function App() {
       <Image source={{ uri: "https://i.gyazo.com/a7438d48207d95dcaf1213b20c97a42c.jpg"}} style={styles.logo} />
       <Text style={styles.instructions}>
         Press the button to upload your plant photo!</Text>
+
+        <TouchableOpacity
+          onPress={() => alert('Hello Plants!')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Upload a photo</Text>
+        </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -29,5 +36,14 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontSize: 18,
     marginHorizontal:5
+  },
+  button: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white'
   }
 });
