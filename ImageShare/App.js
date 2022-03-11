2dc4411ea1b9bbbf966ccfc1d81c1456e77a5c9f
Rob Watkins <rwatkins123@gmail.com>
2022-03-11 12:11:52 -0600

    uploaded photo displaying

--- ImageShare/App.js
+++ ImageShare/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required");
+      return;
+    }
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    setSelectedImage({ localUri: pickerResult.uri })
+    // console.log(pickerResult);
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri}}
+          style={styles.thumbnail}
+          />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: "https://i.gyazo.com/a7438d48207d95dcaf1213b20c97a42c.jpg"}} style={styles.logo} />
@@ -10,7 +40,7 @@ export default function App() {
         Press the button to upload your plant photo!</Text>
 
         <TouchableOpacity
-          onPress={() => alert('Hello Plants!')}
+          onPress={openImagePickerAsync}
           style={styles.button}>
           <Text style={styles.buttonText}>Upload a photo</Text>
         </TouchableOpacity>
@@ -45,5 +75,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: 'white'
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   }
 });
